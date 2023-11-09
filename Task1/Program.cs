fd305cfb097993063fd278e6400bb43c8983c1bc
Yaroslav <kay3@yandex.ru>
2023-11-09 16:58:08 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var textfile = File.ReadAllLines("C:\\Users\\administrator\\Desktop\\text1.txt");
+
+            var stopWatch1 = Stopwatch.StartNew();
+
+            var listArray = new List<string>();
+
+            foreach (var line in textfile)
+            {
+                listArray.Add(line);
+            }
+
+            Console.WriteLine(stopWatch1.Elapsed.TotalMilliseconds);
+
+            stopWatch1.Reset();
+
+            var stopWatch2 = Stopwatch.StartNew();
+
+            var LinkedList = new LinkedList<string>();
+
+            foreach (var line2 in textfile)
+            {
+                LinkedList.AddFirst(line2);
+            }
+
+            Console.WriteLine(stopWatch2.Elapsed.TotalMilliseconds);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            string path = @"C:\Users\administrator\Desktop\text1.txt";
+
+            using (var sr = new StreamReader(path))
+            {
+                var text = sr.ReadToEnd().ToLower();
+
+                text = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+                string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var result = words.GroupBy(x => x)
+                    .Where(x => x.Count() > 1)
+                    .Select(x => new { Word = x.Key, Frequency = x.Count() });
+
+                foreach (var item in result)
+                {
+                    if (item.Frequency > 1)
+                    {
+                        dictionary.Add(item.Word, item.Frequency);
+                    }
+                }
+
+                var word = result.OrderByDescending(x => x.Frequency).Take(10).ToList();
+                foreach (var item in word)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            var orderedWords = dictionary.OrderByDescending(n => n.Value).Take(10).ToList();
+
+            foreach (var item in orderedWords)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
